542b2d95282580c22f048194a53eb227c9dd388f
Gjm <1499513010@qq.com>
2021-01-31 10:15:43 +0800

    登陆功能完成

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,19 @@
 <template>
-$END$
+<div>
+  <el-button type="info" @click="loginout">注销</el-button>
+</div>
 </template>
 
 <script>
 export default {
-name: "Home"
+  name: 'Home',
+  methods: {
+    // 注销登录
+    loginout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/global.css'
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$http = axios
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 new Vue({
   router,
   render: h => h(App)
